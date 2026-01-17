746202d0733dd1477cf5e3ed4a64a38e6bb2c1d6
ParthDevInnovator <makwanaunofficialparth@gmail.com>
2026-01-17 13:44:01 +0530

    week 2 question till now

--- Matrix/set  matrix zero/bruteforce.cpp
+++ Matrix/set  matrix zero/bruteforce.cpp
@@ -1,10 +1,17 @@
 class Solution {
 public:
     void setZeroes(vector<vector<int>>& matrix) {
+        {/*Step 1: Input and Initialize Auxiliary Matrix
+        Start by taking the input matrix.
+        Create an auxiliary matrix ( marks ) of the same size as the input matrix.
+        Initialize all elements in marks as False.*/}
         int m =matrix.size();
         int n =matrix[0].size();
          vector<bool> row(m,false);
          vector<bool>col(n,false);
+         {/*Step 2: Traverse the Input Matrix to Mark Positions
+        Loop through each element in the input matrix.
+        Whenever you encounter a 0 at position (i, j), update marks[i][j] to True.*/}
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
                 if(matrix[i][j]==0){
@@ -13,6 +20,11 @@ public:
                 }
             }
         }
+        {/*Step 3: Update the Original Matrix Using the Marks
+Loop through the matrix again. For each element (i, j):
+If marks[i][k] for any column k in the same row or marks[l][j] for any row l in the same column is True, set matrix[i][j] = 0.
+Step 4: Return the Updated Matrix
+After modifying the matrix, return the output.*/}
             for(int i=0;i<m;i++){
                 for(int j=0;j<n;j++){
                     if(row[i]|| col[j]){
